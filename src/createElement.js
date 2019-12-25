2adb68362b6849de6aa2609f0171fa95df6a940a
duanbowen <dbwencookie@gmail.com>
2019-12-25 22:14:09 +0800

    feat: update createElement

--- src/createElement.js
+++ src/createElement.js
@@ -37,19 +37,88 @@ const createElement = (type, props, ...children) => {
 
 /**
  * 
- * @param {object} fiber 
+ * @param {Fiber} fiber 
+ * @param {object} nextProps 
  * @returns HTMLElement
  */
-const createDom = (fiber) => {
+const createDom = (fiber, nextProps) => {
   const dom = fiber.type === 'TEXT_ELEMENT'
     ? document.createTextNode('')
     : document.createElement(fiber.type);
-  // updateDom(fiber, )
+  
+  // update Dom
+  updateDom(fiber.dom, {}, nextProps)
   return dom;
 }
 
-const updateDom = () => {
-  // TODO
+/*
+fiber = {
+  type: "",
+  props: {
+    children: []
+  },
+  dom: {}, // current dom
+  alternate: {}, // prev dom
+  parent: {}, // parent fiber
+  child: {}, // child fiber
+}
+*/
+/**
+ * @param {HTMLElement} dom 
+ * @param {HTMLElement} newDom 
+ */
+const updateDom = (dom, prevProps, nextProps) => {
+  const isEvent = key => key.startsWith('on');
+  const isProperty = key => !isEvent(key) && key !== 'children';
+
+  // add: isNew
+  // delete: isGone
+  // update: isUpdated
+  const isNew = (prev, next) => (key) => !(key in prev) && (key in next);
+  const isGone = (prev, next) => (key) => (key in prev) && !(key in next);
+  const isUpdated = (prev, next) => (key) => prev[key] !== next[key];
+
+  // delete property
+  Object.keys(prevProps)
+    .filter(isGone(prevProps, nextProps))
+    .forEach(key => {
+    if (isEvent(key)) {
+      const event = key.toLowerCase().substr(2);
+      dom.removeEventListener(event)
+    } else if (isProperty(key)) {
+      dom.removeAttribute(key);
+    }
+  });
+
+  // set new dom property
+  Object.keys(nextProps)
+    .filter(isNew(prevProps, nextProps))
+    .forEach(key => {
+      if (isEvent(key)) {
+        const event = key.toLowerCase().substr(2);
+        dom.addEventListener(event, nextProps[key])
+      } else if (isProperty(key)) {
+        dom.setAttribute(key, nextProps[key]);
+      }
+    });
+
+  // update dom property
+  Object.keys(nextProps)
+    .filter(!isNew(prevProps, nextProps))
+    .filter(!isGone(prevProps, nextProps))
+    .filter(isUpdated(prevProps, nextProps))
+    .forEach(key => {
+      if (isEvent(key)) {
+        const event = key.toLowerCase().substr(2);
+        dom.addEventListener(event, nextProps[key])
+      } else if (isProperty(key)) {
+        dom.setAttribute(key, nextProps[key]);
+      }
+    });
 }
 
-export default createElement;
+export {
+  createElement as default,
+  createDom,
+  updateDom,
+}
--- src/render.js
+++ src/render.js
@@ -1,92 +1,24 @@
-// <div id="root">
-//   <input id="search" />
-//   <a href="/">goto</a>
-// </div>
 
-// const element = {
-//   type: 'div',
-//   props: {
-//     id: 'root',
-//     children: [{
-//       type: 'input',
-//       props: {
-//         id: 'search'
-//       }
-//     }, {
-//       type: 'a',
-//       props: {
-//         href: '/',
-//         value: "goto"
-//       }
-//     }]
-//   }
-// }
+import { createElement, createDom, updateDom } from './createElement.js'
 
 /**
- * 
- * @param {string} text 
- * @returns { type, props }
+ * @param {ReactNode} element
+ * @param {HTMLElement} parentRoot
  */
-const createTextElement = (text) => {
-  return {
-    type: 'TEXT_ELEMENT', // use document.createTextNode('') replace createElement
-    props: {
-      nodeValue: text,
-      children: []
-    }
-  }
-}
-/**
- * 
- * @param {string} type 
- * @param {object} props 
- * @param  {...object} children 
- * @returns { type, props }
- */
-const createElement = (type, props, ...children) => {
-  return {
-    type,
-    props: {
-      ...props,
-      children: children.map(
-        child =>
-          typeof child === 'object'
-          ? createElement(child)
-          : createTextElement(child)
-      )
-    }
-  }
-};
-
-/**
- * 渲染
- * @param {Element} element
- * @param {HTMLElement} container
- */
-const render = (element, container) => {
+const render = (element, parentRoot) => {
   const { type, props } = element;
-  const dom = type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(type);
-  const isListener = name => name.startsWith('on');
-  const isAttrbute = name => !isListener(name) && name !=='children';
-  Object.keys(props)
-    .filter(isListener)
-    .forEach(name => {
-      dom.addEventListener(name.toLowerCase().substring(2), props[name]);
-    }
-  );
-  Object.keys(props)
-    .filter(isAttrbute)
-    .forEach(name => {
-      dom.setAttribute(name, props[name]);
-    }
-  );
-
+  const fiber = {
+    type,
+    props,
+    dom: null
+  }
+  const dom = createDom(fiber, fiber.props)
   if (props.children.length > 0) {
     props.children.forEach((child) => {
       render(child, dom)
     })
   }
-  container.appendChild(dom);
+  parentRoot.appendChild(dom);
 };
 
 export default render;

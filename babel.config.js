16ddb765d742592e392ea71517ee5672c9c30a6c
duanbowen <dbwencookie@gmail.com>
2019-12-28 22:48:21 +0800

    feat: rename babel conifg file

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,5 @@
-module.exports = (api) => {
+module.exports = api => {
   api.cache(true);
-  console.log('api: ', api);
   const presets = [
     [
       "@babel/env",
@@ -9,16 +8,16 @@ module.exports = (api) => {
           edge: "17",
           firefox: "60",
           chrome: "67",
-          safari: "11.1",
+          safari: "11.1"
         },
         useBuiltIns: "usage",
-        corejs: "3.5",
-      },
+        corejs: "3.5"
+      }
     ],
     [
       "@babel/preset-react",
       {
-        "pragma": "MiniReact.createElement", // default pragma is React.createElement
+        pragma: "MiniReact.createElement" // default pragma is React.createElement
         // "pragmaFrag": "DomFrag", // default is React.Fragment
         // "throwIfNamespace": false // defaults to true
       }
@@ -28,5 +27,5 @@ module.exports = (api) => {
   return {
     presets,
     plugins
-  }
-}
+  };
+};
